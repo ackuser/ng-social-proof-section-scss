bd15b767e2e795493b264d26e4feda731c05eda4
Karim Abdeselam <karim.abdeselam@pragsis.com>
2020-11-16 13:06:19 +0100

    Info & Reviews mobile

---
+++ src/app/review/review.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-review',
+  templateUrl: './review.component.html',
+  styleUrls: ['./review.component.scss']
+})
+export class ReviewComponent implements OnInit {
+
+  @Input() desc: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
